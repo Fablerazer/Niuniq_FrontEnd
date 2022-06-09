f2397039e6a6163c9cf35f815c4fa165dec5d884
Faizal H R <rizfianto@gmail.com>
2022-06-09 13:09:06 +0700

    Navigation bar with route

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Logo</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <ul class="nav justify-content-end">
+        <li class="nav-item">
+            <router-link class='nav-link' to="/">Home</router-link>
+          <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
+        </li>
+        <li class="nav-item">
+            <router-link class='nav-link' to="/aboutview">About</router-link>
+          <!-- <a class="nav-link" href="#">About</a> -->
+        </li>
+        <li class="nav-item">
+            <router-link class='nav-link' to="/featureview">Features</router-link>
+          <!-- <a class="nav-link" href="#">Features</a> -->
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="feature">
+    <Navbar />
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    Navbar
+  }
+}
+</script>
