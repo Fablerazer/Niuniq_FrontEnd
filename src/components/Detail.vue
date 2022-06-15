b8a3283d2a75c03b63e1b55b391540c406c4e9b1
Faizal H R <rizfianto@gmail.com>
2022-06-15 17:37:51 +0700

    Added detail

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="row">
-    <div class="col-md-6">
-      <h5 style="color: dodgerblue">
-        <strong>Informasi Produk</strong> <br />
-      </h5>
-      <p class="informasiproduk">
-        Informasi produksi merupakan keterangan dari sebuah produk untuk
-        membuktikan bahwa produk tersebut asli.
-      </p>
+  <div class="row mt-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 style="color: dodgerblue">
+          <strong>Informasi Produk</strong>
+        </h5>
+        <p class="informasiproduk">
+          Informasi produksi merupakan keterangan dari sebuah produk untuk
+          membuktikan bahwa produk tersebut asli.
+        </p>
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Diproduksi Oleh</th>
+              <th scope="col">Tahun Produksi</th>
+              <th scope="col">Lokasi Produksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Rumah Bamboe</td>
+              <td>2018</td>
+              <td>
+                Desa, Gontor 1, Gontor, Kec.Mlarak, Kabupaten Ponorogo, Jawa
+                Timur 63472
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
