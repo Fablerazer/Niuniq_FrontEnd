733d7fb8d2e350e39f5c1e33f62fff0eb8749a7e
Faizal H R <rizfianto@gmail.com>
2022-07-08 17:47:20 +0700

    modified:   src/components/Pagination.vue 	modified:   src/views/TentangViewPenjual.vue

--- src/views/TentangViewPenjual.vue
+++ src/views/TentangViewPenjual.vue
@@ -2,6 +2,7 @@
   <div>
     <CNavbarPenjual />
     <CAboutpage />
+    <CPagination />
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 // @ is an alias to /src
 import CNavbarPenjual from "@/components/NavbarPenjual.vue";
 import CAboutpage from "@/components/Tentang.vue";
+import CPagination from "@/components/Pagination.vue";
 
 export default {
   name: "TentangViewPenjual",
   components: {
     CNavbarPenjual,
     CAboutpage,
+    CPagination
   },
 };
 </script>
