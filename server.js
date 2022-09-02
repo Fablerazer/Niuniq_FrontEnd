8ad95c260e4a15123a41cd837bbca55e962ec70c
Faizal H R <rizfianto@gmail.com>
2022-09-03 06:53:12 +0700

    initial commit

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const path = require('path')
 const app = express()
 
 
-
 app.use('/',serveStatic(path.join(__dirname,'/dist')))
 
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+   })
 
 const port = process.env.PORT || 8080
 
